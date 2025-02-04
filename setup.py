5c2e782121a09b81cfc1011d6998b2804a049644
bmaciolek <bmaciolek@dtiq.com>
2025-02-04 23:37:41 +0100

    setup changes

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Copyright (c) Megvii, Inc. and its affiliates. All Rights Reserved
-
+import os
+os.system("pip install -r requirements.txt")
 import re
 import setuptools
 import glob
@@ -51,14 +52,27 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 
+def get_install_requirements():
+    with open("requirements.txt", "r", encoding="utf-8") as f:
+        reqs = [x.strip() for x in f.read().splitlines()]
+    reqs = [x for x in reqs if not x.startswith("#")]
+    return reqs
+
+
 setuptools.setup(
-    name="yolox",
-    version=version,
+    name="bytetrack",
+    version="0.1.0",
     author="basedet team",
+    url="https://github.com/360iQ/ByteTrack.git",
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
+    install_requires=get_install_requirements(),
+    setup_requires=["wheel"],  # avoid building error when pip is not updated
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,  # include files in MANIFEST.in
     ext_modules=get_extensions(),
     classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
     cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
-    packages=setuptools.find_namespace_packages(),
+    # packages=setuptools.find_namespace_packages(),
 )
