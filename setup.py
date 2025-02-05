c8cc1a423a3d7e397aab3304d4ae5158f6a72212
bmaciolek <bmaciolek@dtiq.com>
2025-02-05 12:44:00 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -2,20 +2,12 @@
 # Copyright (c) Megvii, Inc. and its affiliates. All Rights Reserved
 import os
 os.system("pip install -r requirements.txt")
-import re
 import setuptools
 import torch
 from torch.utils.cpp_extension import CppExtension
 
 torch_ver = [int(x) for x in torch.__version__.split(".")[:2]]
 assert torch_ver >= [1, 3], "Requires PyTorch >= 1.3"
-
-
-with open("bytetrack/__init__.py", "r") as f:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        f.read(), re.MULTILINE
-    ).group(1)
 
 
 with open("README.md", "r") as f:
@@ -29,27 +21,18 @@ def get_install_requirements():
     return reqs
 
 
-def get_package_dir():
-    pkg_dir = {
-        "bytetrack.tools": "tools",
-        "bytetrack.exp.default": "exps/default",
-    }
-    return pkg_dir
-
-
 setuptools.setup(
     name="bytetrack",
     version="0.1.0",
     author="basedet team",
     url="https://github.com/360iQ/ByteTrack.git",
-    packages=setuptools.find_packages() + list(get_package_dir().keys()),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=get_install_requirements(),
-    setup_requires=["wheel"],  # avoid building error when pip is not updated
+    setup_requires=["wheel"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    include_package_data=True,  # include files in MANIFEST.in
+    include_package_data=True,
     classifiers=["Programming Language :: Python :: 3", "Operating System :: OS Independent"],
     cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
-    # packages=setuptools.find_namespace_packages(),
 )
